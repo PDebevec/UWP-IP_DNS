9d17809b5b5ab770769397cdf26f62287858e86b
Peter Debevec <peterusdebevec@gmail.com>
2023-06-19 19:12:15 +0200

    ne vem

--- UWP-whois/UWP-whois/MainPage.xaml.cs
+++ UWP-whois/UWP-whois/MainPage.xaml.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,6 +30,61 @@ namespace UWP_whois
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (domena.Text == "")
+            {
+                if (ip.Text == "")
+                {
+                    return;
+                }
+                else
+                {
+                    try
+                    {
+                        using (HttpClient client = new HttpClient())
+                        {
+                            test.Text = ip.Text;
+                            client.DefaultRequestHeaders.Add("User-Agent", "ipapi.co/#c-sharp-v1.03");
+                            string apiUrl = "https://ipapi.co/"+ ip.Text +"/json/";
+
+                            HttpResponseMessage response = await client.GetAsync(apiUrl);
+                            test.Text = response.StatusCode.ToString();
+                            response.EnsureSuccessStatusCode();
+
+                            string jsonResponse = await response.Content.ReadAsStringAsync();
+                            test.Text = jsonResponse;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"An error occurred: {ex.Message}");
+                    }
+                }
+            }
+            else
+            {
+                try
+                {
+                    using (HttpClient client = new HttpClient())
+                    {
+                        test.Text = domena.Text;
+                        string apiUrl = "https://networkcalc.com/api/dns/lookup/" + domena.Text;
+
+                        HttpResponseMessage response = await client.GetAsync(apiUrl);
+                        response.EnsureSuccessStatusCode();
+
+                        string jsonResponse = await response.Content.ReadAsStringAsync();
+                        test.Text = jsonResponse;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+            }
         }
     }
 }
